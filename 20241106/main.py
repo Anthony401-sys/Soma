919137f11f28538907745ee91b2c7da671f43265
Anthony <anthony.pf@aluno.ifsc.edu.br>
2024-11-06 13:19:00 -0300

    matrizez

--- 20241106/main.py
+++ 20241106/main.py
@@ -7,12 +7,41 @@ def CriaMatriz(NumLinhas, NumColunas):
 	return matrizA
 
 def ImprimirMatriz(M): 
-	NumLinhas = len(M)
-	NumColunas = len(M[0])
+	VerificaMatriz = EhMatriz(M)
+	if (VerificaMatriz != False): 
+		NumLinhas, NumColunas = VerificaMatriz
+	else: 
+		print("Nao foi recebida uma matriz em PreencheMatrizPorColunas(M)")
 	for i in range(NumLinhas): 
 		for j in range(NumColunas): 
 			print(M[i][j], end = " ")
 		print()	
+
+def EhMatriz(M): 	
+	if type(M) == list: 
+		nlinhas = len(M)
+		if type(M[0]) == list: 
+			ncols = len(M[0])
+			return nlinhas, ncols
+		else: 
+			print("Vetor de 1 dimensao")	
+			return False
+	else: 
+		print("Entrada não é vetor")
+		return False	
+
+
+def PreencheMatrizPorColunas(M): 
+	VerificaMatriz = EhMatriz(M)
+	if (VerificaMatriz != False): 
+		NumLinhas, NumColunas = VerificaMatriz
+	else: 
+		print("Nao foi recebida uma matriz em PreencheMatrizPorColunas(M)")
+	print("Passagem por EhMatriz() feita")		
+	for i in range(NumLinhas): 
+		for j in range(NumColunas): 
+			M[i][j]=j*NumLinhas+i+1
+	return M		
 
 def PreencheMatrizPorLinhas(M): 
 	NumLinhas = len(M)
@@ -21,11 +50,43 @@ def PreencheMatrizPorLinhas(M):
 	for i in range(NumLinhas): 
 		for j in range(NumColunas): 
 			M[i][j]=i*NumColunas+j+1
-	ImprimirMatriz(M)
 	return M		
 
+
+def CompreensaoDeMatrizes(NumLinhas,NumColunas): 
+	print("Percorrimento por linha")
+	A = [[i*NumColunas+j+1 for j in range(NumColunas)] for i in range(NumLinhas)]
+	return A
 	
+		
 
 if __name__ == "__main__": 
-	A=CriaMatriz(4,3)			
-	PreencheMatrizPorLinhas(A)
+	NumLinhas = 4
+	NumColunas = 3
+	#A=CriaMatriz(NumLinhas,NumColunas)			
+	#PreencheMatrizPorLinhas(A)	
+	#PreencheMatrizPorColunas(A)
+	#A = [[i for i in range(1,9)] for j in range (5)]
+	#ImprimirMatriz(A)
+	#A = [i for i in range(5)]
+	#print(A)	
+	A = CompreensaoDeMatrizes(NumLinhas, NumColunas)	
+	ImprimirMatriz(A)
+	#Compreensao de Matrizes gera uma matriz do zero.
+	#Nao precisa receber matriz pronta. 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+	
